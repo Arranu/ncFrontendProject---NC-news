407e9049f46eea0bb282e1f4af90b22501ef4719
Arranu <60885158+Arranu@users.noreply.github.com>
2024-02-08 18:05:39 +0000

    add loggedInUser as usecontext

---
+++ src/context/users.jsx
@@ -0,0 +1,12 @@
+import { createContext, useState } from 'react';
+
+export const UserContext = createContext();
+export const LoggedInUser = ({user}) =>{
+    const [user,setUser]=useState('tickle122') //default user for testing/presentation - need  login system
+
+    return(
+        <UserContext.Provider value ={{user,setUser}}>
+            {user}
+        </UserContext.Provider>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom'
+import { UserContext } from './context/users.jsx'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    <UserContext>
+        <App />
+    </UserContext>
   </BrowserRouter>
   )
